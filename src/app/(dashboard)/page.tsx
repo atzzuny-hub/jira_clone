34cfe47e4cc03a476b1a3a85cc7cf154f353ef67
jungwonjoo <wonjoo88@nate.com>
2025-09-26 20:36:45 +0900

    Appwrite로 워크스페이스 생성 기능 구현

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,20 +1,17 @@
 import { redirect } from "next/navigation";
-
 import { getCurrent } from "@/features/auth/actions";
-import { UserButton } from "@/features/auth/components/user-button";
+import { CreateWorkspaceForm } from "@/features/workspaces/component/create-workspace-form";
 
 export default async function Home() {
 
-	const user = await getCurrent();
-
-	console.log(user);
-	
+	const user = await getCurrent();	
 
 	if(!user) redirect("/sign-in")
   
 	return (
-		<div style={{padding:20}}>
-			홈페이지
+		<div className="bg-neutral-500 p-4 h-full">
+			<CreateWorkspaceForm/>
 		</div>
+
 	);	
 }
