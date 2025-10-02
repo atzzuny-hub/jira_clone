3aa6c01440ee76aa0368810903c6ec674c24780d
wonjoo88 <wonjoo88@revelogis.com>
2025-10-02 10:02:00 +0900

    워크스페이스 멤버십 시스템

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,5 @@ export const PROJECT_ID = process.env.NEXT_PUBLIC_APPWRITE_PROJECT!;
 
 export const DATABASE_ID = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!;
 export const WORKSPACES_ID = process.env.NEXT_PUBLIC_APPWRITE_WORKSPACES_ID!;
+export const MEMBERS_ID = process.env.NEXT_PUBLIC_APPWRITE_MEMBERS_ID!;
 export const IMAGES_BUCKET_ID = process.env.NEXT_PUBLIC_APPWRITE_IMAGES_ID!;
