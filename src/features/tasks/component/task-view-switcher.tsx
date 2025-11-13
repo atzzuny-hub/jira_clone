bbc62f17949950d99c2f677ebe96f24cf6332169
jungwonjoo <wonjoo88@nate.com>
2025-11-13 19:40:21 +0900

    Task View Switcher와 Data Filters 구현

--- src/features/tasks/component/task-view-switcher.tsx
+++ src/features/tasks/component/task-view-switcher.tsx
@@ -1,15 +1,51 @@
 'use client'
 
-import DottedSeparator from "@/components/dottedSeparator"
+import { Loader, PlusIcon } from "lucide-react"
+import { useQueryState } from "nuqs"
+
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id"
+
 import { Button } from "@/components/ui/button"
+import DottedSeparator from "@/components/dottedSeparator"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { PlusIcon } from "lucide-react"
+
+import { useGetTasks } from "../api/use-get-task"
 import { useCreateTaskModal } from "../hooks/use-create-task-modal"
+import { DataFilters } from "./data-filters"
+import { useTaskFilters } from "../hooks/use-task-filters"
 
 export const TaskViewSwitcher = () => {
+
+    const [{
+        status,
+        assigneeId,
+        projectId,
+        dueDate
+    }] = useTaskFilters();
+
+    const [view, setView] = useQueryState("task-view", {
+        defaultValue: "table"
+    })
+ 
+    const workspaceId = useWorkspaceId()   
     const {open} = useCreateTaskModal();
+
+    // const { data:tasks, isLoading: isLoadingTasks } = useGetTasks({workspaceId})
+    
+    const { data:tasks, isLoading: isLoadingTasks } = useGetTasks({
+        workspaceId,
+        status,
+        assigneeId,
+        projectId,
+        dueDate
+    })
+
     return (
-        <Tabs className="flex-1 w-full border rounded-lg">
+        <Tabs 
+            defaultValue={view}
+            onValueChange={setView}
+            className="flex-1 w-full border rounded-lg"
+        >
         <div className="h-full flex flex-col overflow-auto p-4">
             <div className="flex flex-col gap-y-2 lg:flex-row justify-between items-center">
             <TabsList className="flex gap-2 w-full lg:w-auto">
@@ -33,19 +69,25 @@ export const TaskViewSwitcher = () => {
             </Button>
             </div>
             <DottedSeparator className="my-4"/>
-            Data filters
+                <DataFilters/>
             <DottedSeparator className="my-4"/>
-            <>
-                <TabsContent value="table" className="mt-0">
-                    Data Table
-                </TabsContent>
-                <TabsContent value="kanban" className="mt-0">
-                    Data Kanban
-                </TabsContent>
-                <TabsContent value="calendar" className="mt-0">
-                    Data Calendar
-                </TabsContent>
-            </>
+            {isLoadingTasks ? (
+                <div className="w-full border rounded-lg h-[200px] flex flex-col items-center justify-center">
+                    <Loader className="size-5 animate-spin text-muted-foreground"/>
+                </div>
+            ) : (
+                <>
+                    <TabsContent value="table" className="mt-0">
+                        {JSON.stringify(tasks)}
+                    </TabsContent>
+                    <TabsContent value="kanban" className="mt-0">
+                        {JSON.stringify(tasks)}
+                    </TabsContent>
+                    <TabsContent value="calendar" className="mt-0">
+                        {JSON.stringify(tasks)}
+                    </TabsContent>
+                </>
+            )}
         </div>
         </Tabs>
     )
