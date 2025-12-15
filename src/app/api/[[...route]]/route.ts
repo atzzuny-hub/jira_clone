7398bafe9be8102eee3115513f38ba008d1b93a6
jungwonjoo <wonjoo88@nate.com>
2025-12-15 17:49:04 +0900

    CORS error

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,5 +1,6 @@
 import {Hono} from 'hono'
 import {handle} from 'hono/vercel'
+import { cors } from 'hono/cors';
 
 import auth from '@/features/auth/server/route'
 import workspaces from '@/features/workspaces/server/route'
@@ -8,6 +9,17 @@ import projects from '@/features/projects/server/route'
 import tasks from '@/features/tasks/server/route'
 
 const app = new Hono().basePath("/api");
+
+app.use(
+  '*',
+  cors({
+    origin: 'https://jira-clone-pearl-three.vercel.app',
+    allowMethods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  })
+);
+
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const routes = app
